725ac87a05fbcf0017efb11517f41508944f58b0
cabosw <abigail.oswald475@gmail.com>
2021-11-22 16:09:48 -0800

    function for password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,15 @@
 // Assignment code here
+var lengthPrompt = function() {
+  var lengthInput = window.prompt("Enter a number for password length. Minimum 8 characters/maximum 128 characters.");
+  if (lengthInput < 8 || lengthInput > 128);
+    window.alert("Length needs to be between 8-128 characters. Try again");
+    return lengthPrompt();
+}
+
+lengthPrompt();
 
 
-// Get references to the #generate element
+  // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
