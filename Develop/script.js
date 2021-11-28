5daacbaa9d4843d2ab4fc8c353d5242674e842ee
cabosw <abigail.oswald475@gmail.com>
2021-11-28 00:54:08 -0800

    removed unnecessary functions, set criteria prompts as variables in generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,32 @@
-// Assignment code here
-var lengthPrompt = function() {
-  do {
-  var lengthInput = window.prompt("Enter a number for password length. Minimum 8 characters/maximum 128 characters."); }
-  while (lengthInput < 8 || lengthInput > 128 || !lengthInput || isNaN(lengthInput)); {
-  }
-    return lengthInput;
-  };
-
-console.log(lengthPrompt());
-
-var upperCasePrompt = function() {
-    var upperCase = window.confirm("Include capital letters?"); 
-    return upperCase;
-  };
-
-console.log(upperCasePrompt());
-
-var lowerCasePrompt = function() {
-  var lowerCase = window.confirm("Include lower case letters?");
-  return lowerCase;
-}
-
-console.log(lowerCasePrompt());
-
-var numericPrompt = function() {
-  var numerics = window.confirm("Include numeric values?");
-  return numerics;
-}
 
 
-console.log(numericPrompt());
-
-var specialCharactersPrompt = function() {
-  var specialCharacter = window.confirm("Include special characters?");
-  return specialCharacter;
-}
-
-console.log(specialCharactersPrompt());
-
-var lowerCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//variable arrays for possible password content
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ",", "<", ".", ">", "/", "?", ";", ":", "[", "]", "{", "}"];
 
-var megaArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ",", "<", ".", ">", "/", "?", ";", ":", "[", "]", "{", "}"];
+//variables for confirm prompts of password criteria
+var lengthConfirm;
+var upperCaseConfirm;
+var lowerCaseConfirm;
+var numericsConfirm;
+var specialCharactersConfirm;
+
+
+//generatePassword function start
+function generatePassword() {
+  var lengthConfirm = window.prompt("Enter a number for password length. Minimum 8 characters/maximum 128 characters.");
+  do {
+    lengthConfirm = window.prompt("Enter a number for password length. Minimum 8 characters/maximum 128 characters.");
+  }
+  while (lengthConfirm < 8 || lengthConfirm > 128 || lengthConfirm === null || isNaN(lengthConfirm));
+  
+  var upperCaseConfirm = window.confirm("Include capital letters?");
+  var lowerCaseConfirm = window.confirm("Include lower case letters?");
+  var numericsConfirm = window.confirm("Include numeric values?");
+  var specialCharactersConfirm = window.confirm("Include special characters?");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -58,4 +41,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
